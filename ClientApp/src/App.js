1d1f4b666f4f417f37ea077e124c7f3ccfea9612
Blessed <fernqndoivan@gmail.com>
2022-09-23 22:54:22 -0600

    1

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -100,7 +100,7 @@ const App = () => {
                 <footer className="sticky-footer bg-white">
                     <div className="container my-auto">
                         <div className="copyright text-center my-auto">
-                            <span>Copyright &copy; Your Website 2020</span>
+                            <span>Copyright &copy; UNIVERSIDAD MARIANO GALVEZ DE GUATEMALA</span>
                         </div>
                     </div>
                 </footer>
